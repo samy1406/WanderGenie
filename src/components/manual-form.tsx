362e8d4c199cd99b3c534c2dc1a6df42d44a60de
Vatsal Patel <vatsalviradiya83@gmail.com>
2025-11-01 19:13:51 +0000

    Still More Filter's Box and Interest's text box is not aligned horizonta

--- src/components/manual-form.tsx
+++ src/components/manual-form.tsx
@@ -310,7 +310,7 @@ export function ManualForm({ isLoading, form, tripType, setTripType }: ManualFor
                     )}
                 />
                  <div className="flex flex-col gap-2">
-                    <FormLabel>&nbsp;</FormLabel>
+                    <div><FormLabel>&nbsp;</FormLabel></div>
                     <Popover>
                         <PopoverTrigger asChild>
                             <Button variant="outline" size="sm" className="w-full">
